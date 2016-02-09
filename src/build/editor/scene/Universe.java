7d7cf7dee2d43dc16baac2ca35d93f91771852a1
Aleman778 <aleman778.maintheria@gmail.com>
2016-02-09 22:39:10 +0100

    Updated Universe

--- src/build/editor/scene/Universe.java
+++ src/build/editor/scene/Universe.java
@@ -1,25 +1,12 @@
 package build.editor.scene;
 
-import com.sun.j3d.utils.geometry.ColorCube;
-import com.sun.j3d.utils.geometry.Cone;
-import java.awt.Color;
 import java.io.File;
-import java.lang.reflect.Field;
-import javax.media.j3d.Appearance;
-import javax.media.j3d.Background;
-import javax.media.j3d.BoundingSphere;
 import javax.media.j3d.BranchGroup;
 import javax.media.j3d.Group;
 import javax.media.j3d.Locale;
-import javax.media.j3d.Material;
 import javax.media.j3d.Node;
-import javax.media.j3d.RenderingAttributes;
-import javax.media.j3d.Transform3D;
 import javax.media.j3d.TransformGroup;
 import javax.media.j3d.VirtualUniverse;
-import javax.vecmath.Color3f;
-import javax.vecmath.Point3d;
-import javax.vecmath.Vector3f;
 
 public class Universe extends VirtualUniverse {
     
@@ -34,34 +21,9 @@ public class Universe extends VirtualUniverse {
         root.setCapability(Group.ALLOW_CHILDREN_READ);
         root.setCapability(Group.ALLOW_CHILDREN_WRITE);
         
-        //DEBUG REMOVE LATER
-        TexturedFloor floor = new TexturedFloor();
-        ColorCube cube = new ColorCube();
-        
-        Transform3D trans = new Transform3D();
-        trans.setTranslation(new Vector3f(0, 20, 0));
-        TransformGroup group = new TransformGroup(trans);
-        
-        Appearance appearance = new Appearance();
-        cube.setAppearance(appearance);
-        Background back = new Background(new Color3f(0.2f, 0.5f, 1.0f));
-        BoundingSphere sphere = new BoundingSphere(new Point3d(0, 0, 0), 100);
-        back.setApplicationBounds(sphere);
-        Transform3D transform = new Transform3D();
-        transform.setTranslation(new Vector3f(0, 1, 0));
-        test = new TransformGroup(transform);
-        test.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-        test.setCapability(TransformGroup.ALLOW_TRANSFORM_READ);
-        test.addChild(cube);
-        root.addChild(test);
-        root.addChild(group);
-        root.addChild(floor);
-        root.addChild(back);
-        
         //Locale
         locale = new Locale(this);
         locale.addBranchGraph(root);
-        
     }
     
     public void addSceneView(SceneView view) {
@@ -90,5 +52,10 @@ public class Universe extends VirtualUniverse {
     
     public void export(File file) {
         
+    }
+
+    @Override
+    public String toString() {
+        return "Universe";
     }
 }
