17d313db1ddbc4bc71aa4b367e0c5cb9cc33d736
Aleman778 <aleman778.maintheria@gmail.com>
2016-03-17 00:00:36 +0100

    Resource Tree

---
+++ src/build/editor/resource/JTreeResources.java
@@ -0,0 +1,52 @@
+package build.editor.resource;
+
+import build.editor.ui.acomponents.ATree;
+import java.io.File;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import javax.swing.tree.TreeModel;
+
+public class JTreeResources extends ATree {
+
+    public static File projectRoot, resourceFolder;
+    public static final JTreeResources instance = new JTreeResources();
+    
+    //Singleton
+    private JTreeResources() {
+        setCellRenderer(new ResourceGraphRenderer());
+    }
+    
+    public void setProjectRoot(File root, File resfolder) {
+        JTreeResources.projectRoot = root;
+        JTreeResources.resourceFolder = resfolder;
+        
+        buildGraph(resfolder);
+    }
+    
+    public void buildGraph(File file) {
+        if (file.isDirectory()) {
+            DefaultMutableTreeNode node = new DefaultMutableTreeNode(file.getName());
+            buildFolder(node, file);
+            TreeModel model = new DefaultTreeModel(node);
+            setModel(model);
+        }
+        
+    }
+    
+    private void buildFolder(DefaultMutableTreeNode parent, File file) {
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+
+            if (files == null)
+                return;
+            
+            for (File f: files) {
+                if (f.isDirectory()) {
+                    DefaultMutableTreeNode node = new DefaultMutableTreeNode(f.getName());
+                    buildFolder(node, f);
+                }
+            }
+        }
+    }
+    
+}
---
+++ src/build/editor/resource/ResourceGraphRenderer.java
@@ -0,0 +1,48 @@
+package build.editor.resource;
+
+import build.editor.manager.ThemeManager;
+import build.editor.scene.Universe;
+import java.awt.Component;
+import javax.media.j3d.BranchGroup;
+import javax.media.j3d.Group;
+import javax.media.j3d.Locale;
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.TransformGroup;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JTree;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeCellRenderer;
+
+public class ResourceGraphRenderer extends DefaultTreeCellRenderer {
+
+    private static final ImageIcon ICON_CLOSED = new ImageIcon("res/gui/icons/iconFolderClosed.png");
+    private static final ImageIcon ICON_OPEN = new ImageIcon("res/gui/icons/iconFolderOpen.png");
+    
+    public ResourceGraphRenderer() {
+        setClosedIcon(ICON_CLOSED);
+        setOpenIcon(ICON_OPEN);
+    }
+
+    @Override
+    public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selection, boolean expanded, boolean leaf, int row, boolean hasFocus) {
+        super.getTreeCellRendererComponent(tree, value, selection, expanded, leaf, row, false);
+        
+        if (!selected) {
+            setOpaque(true);
+        } else {
+            setOpaque(false);
+        }
+        setBorder(BorderFactory.createEmptyBorder());
+        setBackground(ThemeManager.COLOR_PANEL);
+        setForeground(ThemeManager.COLOR_FOREGROUND);
+        
+        DefaultMutableTreeNode node = (DefaultMutableTreeNode) value;
+        
+        if (node.isLeaf()) {
+            setIcon(ICON_CLOSED);
+        }
+        
+        return this;
+    }
+}
